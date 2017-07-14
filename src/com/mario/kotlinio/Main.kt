2a8e9e4db4db6a7b78221ba94d9a2b457f981405
Mario Feles dos Santos Junior <mario_feles@live.com>
2017-07-14 18:01:30 -0300

    Initial commit

---
+++ src/com/mario/kotlinio/Main.kt
@@ -0,0 +1,34 @@
+package com.mario.kotlinio
+
+import java.math.BigDecimal
+
+/**
+ * Created by mario on 14/07/17.
+ */
+
+fun main(args: Array<String>) {
+
+    val tickets = Money(BigDecimal(100),"$")
+    val popcorn = tickets.copy(amount = BigDecimal(500), currency = "EUR")
+
+    sendPayment(message = "Good Mario",money = tickets)
+
+    if (tickets != popcorn){
+        print("They are different!")
+    }
+
+}
+
+fun sendPayment(money: Money,message: String = ""){
+    println("Sending ${money.amount}")
+}
+
+fun sum(x: Int,y: Int) = x + y
+
+fun conveterToDollars(money: Money) = when (money.currency){
+    "$" ->  money
+    "EUR" ->  Money(money.amount * BigDecimal(1.10),"$")
+    else -> throw IllegalArgumentException("not the currency you're interested")
+}
+
+fun BigDecimal.percent(percentage: Int) = this.multiply(java.math.BigDecimal(percentage)).divide(BigDecimal(1000))
---
+++ src/com/mario/kotlinio/Money.kt
@@ -0,0 +1,8 @@
+package com.mario.kotlinio
+
+import java.math.BigDecimal
+
+/**
+ * Created by mario on 14/07/17.
+ */
+data class Money(val amount :BigDecimal, val currency: String)
